ea188269fdfdbc128fd281924fd463c92358f757
Hakan Sener <h-akan@hotmail.fr>
2016-03-17 16:37:58 +0100

    remose useless comments

--- src/arbrebinaire/Visiteur.java
+++ src/arbrebinaire/Visiteur.java
@@ -1,9 +1,7 @@
 package arbrebinaire;
 
 public interface Visiteur {
-	
-	//public void visiterOperateurUnaire(OperateurUnaire operateurUnaire);
-	//public void visiterOperateurBinaire(OperateurBinaire operateurBinaire);
+
 	public void visiterNegation(Negation negation);
 	public void visiterAddition(Addition addition);
 	public void visiterMultiplication(Multiplication multiplication);
